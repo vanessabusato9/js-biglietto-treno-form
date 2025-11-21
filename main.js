9678d114d033b764aeec24a026a8b5607d14da68
vanessa <vane.busato9@gmail.com>
2025-11-21 13:28:43 +0100

    aggiunto costanti e addEventListener

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const km = parseInt(prompt("Quanti km vuoi percorrere?"));
+/*const km = parseInt(prompt("Quanti km vuoi percorrere?"));
 const eta = parseInt(prompt("Quanti anni hai?"));
 const prezzoBiglietto = km * 0.21;
 let prezzoFinale = prezzoBiglietto;
@@ -17,4 +17,27 @@ else {
 }
 
 console.log(prezzoFinale);
-document.getElementById("totale").innerText = `il prezzo Finale è ${prezzoFinale.toFixed(2)}`;
+document.getElementById("totale").innerText = `il prezzo Finale è ${prezzoFinale.toFixed(2)}`;*/
+
+document.getElementById("btn-genera").addEventListener("click", function(e) {
+
+    e.preventDefault(); //impedisce il refresh del form - verificare perchè fa refresh cmq
+
+
+    const distanzaElemento = document.getElementById("distanza");
+    const etaElemento = document.getElementById("eta");
+    const prezzoPienoElemento = document.getElementById("prezzo-pieno");
+    const scontoElemento = document.getElementById("sconto");
+    const PrezzoScontatoElemento = document.getElementById("prezzo-scontato");
+
+    if (eta < 18) {
+        prezzoFinale = prezzoBiglietto * 0.80;
+    }
+    else if (eta >= 65) {
+        prezzoFinale = prezzoBiglietto * 0.60;
+    }
+    else {
+        prezzoFinale = prezzoFinale;
+    }
+
+}
